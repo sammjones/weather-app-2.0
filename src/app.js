78ba86ff9c6071a80a42f1b424afa7e4c8c372d1
Sami Jones <62959599+sammjones@users.noreply.github.com>
2020-11-13 19:16:21 -0700

    Showing the current temperature and sky conditions. (added in OpenWeather Maps API)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+let apiKey = "1768d882e21eece70a5c2f0f1301e790";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=1768d882e21eece70a5c2f0f1301e790&units=metric";
 
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let skystatusElement = document.querySelector("#skystatus");
+  skystatusElement.innerHTML = response.data.weather[0].description;
+
+  temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+axios.get(apiUrl).then(displayTemperature);
