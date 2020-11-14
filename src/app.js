e05c03e35b7fce1a6a6876948a5b478d35c86803
Sami Jones <62959599+sammjones@users.noreply.github.com>
2020-11-14 10:32:45 -0700

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,24 +1,44 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  let skystatusElement = document.querySelector("#skystatus");
+  let windElement = document.querySelector("#wind");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement = document.querySelector("#temperature");
+  let dateElement = document.querySelector("#date");
+  cityElement.innerHTML = response.data.name;
+
+  skystatusElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
 let apiKey = "1768d882e21eece70a5c2f0f1301e790";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=1768d882e21eece70a5c2f0f1301e790&units=metric";
-
-function displayTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.main.temp);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
-
-  let skystatusElement = document.querySelector("#skystatus");
-  skystatusElement.innerHTML = response.data.weather[0].description;
-
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = Math.round(response.data.main.humidity);
-
-  temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-}
 
 axios.get(apiUrl).then(displayTemperature);
